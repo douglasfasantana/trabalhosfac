a9f74e6770559f98320db21bdf44039670a32218
douglasfasantana <83181628+douglasfasantana@users.noreply.github.com>
2021-05-27 21:39:51 -0300

    Update L1E1.cpp
    adicionando os enunciados

--- L1E1.cpp
+++ L1E1.cpp
@@ -1,4 +1,4 @@
-// Lista 1: Exercício 1
+// Lista 1: ExercÃ­cio 1 - Construa um programa que inverta o nÃºmero 745.
 
 #include <iostream>
 #include <cstdlib>
@@ -6,14 +6,14 @@ using namespace std;
 
 int main ()
 {
-  int num, uni, dez, cent, inver; // Variáveis
-  num = 745; // declarando o número 745 
+  int num, uni, dez, cent, inver; // VariÃ¡veis
+  num = 745; // declarando o nÃºmero 745 
   
-  // Separando os números e já invertendo
+  // Separando os nÃºmeros e jÃ¡ invertendo
   uni = num /100; // nessa conta estarei pegando o numero que esta na centena, para tornar ele em unidade ( Ex: 100 / 100 = 1)
-  dez = num /10 % 10; // Pegando o numero que está na dezena e tornando ele uma unidade(1 numero só). (Ex 123 / 10 = 12,3 % 10 = 2)
-  cent = num % 10; // Pegando o numero que está na unidade e tornando ele uma unidade tbm. (Ex: 123 % 10 = 3)
-  inver = cent * 100 + dez * 10 + uni * 1; //juntando os numeros já invertidos e colocando dentro de uma variavel
+  dez = num /10 % 10; // Pegando o numero que estÃ¡ na dezena e tornando ele uma unidade(1 numero sÃ³). (Ex 123 / 10 = 12,3 % 10 = 2)
+  cent = num % 10; // Pegando o numero que estÃ¡ na unidade e tornando ele uma unidade tbm. (Ex: 123 % 10 = 3)
+  inver = cent * 100 + dez * 10 + uni * 1; //juntando os numeros jÃ¡ invertidos e colocando dentro de uma variavel
   // cent = antiga unidade // dez = antiga dezena // uni = antiga centena
   
   cout <<"O numero invertido e "<<inver<<".\n\n";
