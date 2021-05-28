ddd7d80e617353e49c3666e1374cae71bde42cf6
douglasfasantana <83181628+douglasfasantana@users.noreply.github.com>
2021-05-27 21:45:34 -0300

    Update L1E9.cpp

--- L1E9.cpp
+++ L1E9.cpp
@@ -1,4 +1,6 @@
-// Lista 1: ExercÌcio 9
+// Lista 1: Exerc√≠cio 9
+// Construa um programa que exiba a √°rea e o per√≠metro de um ret√¢ngulo que tem seus lados
+// informados pelo usu√°rio.
 
 #include <iostream>
 #include <cstdlib>
@@ -6,14 +8,14 @@ using namespace std;
 
 int main ()
 {
-	float l, h, area, peri; // Sendo um retangulo tendo 2 lados iguais. voce sÛ precisar· de sua altura e largura. As outras variaveis s„o para extrair a area e o perimetro do retangulo.
+	float l, h, area, peri; // Sendo um retangulo tendo 2 lados iguais. voce s√≥ precisar√° de sua altura e largura. As outras variaveis s√£o para extrair a area e o perimetro do retangulo.
 	
 	cout <<"Vamos construir um retangulo.\nDigite a largura: ";
 		cin >>l;
 	cout <<"Digite a altura: ";
 		cin >>h;
-	area = l*h; // Fazendo a express„o matem·tica: ¡rea = Largura * Altura
-	peri = l*2 + h*2; // Fazendo a express„o matem·tica: Perimetro = Largura * 2 + Altura * 2 (Largura 1 + Largura 2 + Altura 1 + Altura 2).
+	area = l*h; // Fazendo a express√£o matem√°tica: √Årea = Largura * Altura
+	peri = l*2 + h*2; // Fazendo a express√£o matem√°tica: Perimetro = Largura * 2 + Altura * 2 (Largura 1 + Largura 2 + Altura 1 + Altura 2).
 	
 	cout <<"\nA area do seu retangulo e: "<<area<<".\nO perimetro do seu retangulo e :"<<peri<<".\n\n";
 	system ("Pause");
